36b20704c8fcf4d7868a0352cbf57e757ae59f24
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2023-12-06 01:26:23 +0300

    Add files via upload

---
+++ Basic Addition Project.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	double number1, number2, sum;
+
+	cout << "Enter two number to add : " << endl;
+	cin >> number1 >> number2;
+	sum = number1 + number2;
+
+	cout << fixed << setprecision(2) << sum;
+
+	return 0;
+	
+}
+
